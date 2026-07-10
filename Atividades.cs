fcbd68b9811ac12cb370f40d0460f151d2cdceb1
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-07-09 21:56:11 -0300

    q2

--- Atividades.cs
+++ Atividades.cs
@@ -18,7 +18,31 @@ public static class Atividades
     }
     public static void Atividade02()
     {
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        double[] notas = new double[5];
+
+        double soma = 0;
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.Write($"Informe a nota do aluno {i + 1}: ");
+            notas[i] = double.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < notas.Length; i++)
+        {
+            soma += notas[i];
+        }
+        double media = soma / notas.Length;
+
+        Console.WriteLine();
+        Console.WriteLine($"Média da turma: {media:F2}");
+    }
+}
     }
     public static void Atividade03()
     {
